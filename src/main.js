35e1740644409c24dcad6e5590c8be9b827551ac
SolomiiaKrip02 <solomiya.kr08@gmail.com>
2025-08-24 11:29:30 +0300

    Update main.js

---
+++ src/main.js
@@ -0,0 +1,32 @@
+import fetchData from './js/pixabay-api';
+import {
+  createGallery,
+  clearGallery,
+  showLoader,
+  hideLoader,
+} from './js/render-functions';
+
+const form = document.getElementById('search-form');
+
+form.addEventListener('submit', async e => {
+  e.preventDefault();
+  const query = e.target.elements['search-text'].value.trim();
+  if (!query) return;
+
+  clearGallery();
+  showLoader();
+
+  try {
+    const data = await fetchData(query);
+    if (data.hits.length === 0) {
+      alert('No images found. Try another search.');
+    } else {
+      createGallery(data.hits);
+    }
+  } catch (error) {
+    alert('Error fetching images');
+    console.error(error);
+  } finally {
+    hideLoader();
+  }
+});
